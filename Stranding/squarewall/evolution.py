06bf959de93e4aaa94a7b6851b14c16f7e3810cf
eolicneb <43392473+eolicneb@users.noreply.github.com>
2019-07-12 08:51:09 -0300

    Add files via upload

--- Stranding/squarewall/evolution.py
+++ Stranding/squarewall/evolution.py
@@ -2,8 +2,8 @@ import squarewall as sq
 from thinking import Brain
 
 schema = (4, 5, 5, 4)
-population = 50
-reproductions = population // 3 + 1
+population = 100
+reproductions = population // 4 + 1
 
 class Window(object):
     width = 600
@@ -27,10 +27,10 @@ sq.Fauna.zoo = [ sq.Creature(movement=RegisteredMov(pos=[100,100], box=Window, b
 
 dt = 5.
 sq.Park.dt = dt
-for iteration in range(5):
+for iteration in range(1000):
     print('Iteration ', iteration)
 
-    for _ in range(100):
+    for _ in range(1000):
         sq.Movement.inc_t(dt)
         for c in sq.Fauna.zoo:
             c.move(dt)
@@ -42,12 +42,15 @@ for iteration in range(5):
         c.mov.register.sort()
         darwin.append((c.mov.register[-1], ix))
         for reg in c.mov.register[:-3:-1]:
-            print('  {:8.0f} {:4d} d={:8.3f} t={:6.3f}'.format(*reg))
+            print('  {:12.0f} {:4d} d={:8.3f} t={:6.3f}'.format(*reg))
     darwin.sort(reverse=True)
     # sq.Fauna.zoo[darwin[0][1]] = sq.Creature(movement=RegisteredMov(pos=[100,100], box=Window, brain=Brain(schema=schema)))
     # sq.Fauna.zoo[darwin[1][1]] = sq.Creature(movement=RegisteredMov(pos=[100,100], box=Window, brain=Brain(schema=schema)))
     for d in range(reproductions):
-        sq.Fauna.zoo[darwin[d+reproductions][1]].mov.brain.layers = sq.Fauna.zoo[darwin[d][1]].mov.brain.layers
+        sq.Fauna.zoo[darwin[d+reproductions][1]] = sq.Creature(movement=RegisteredMov(pos=[100,100], 
+                                                                        box=Window, 
+                                                                        brain=Brain(schema=schema, layers=sq.Fauna.zoo[darwin[d][1]].mov.brain.layers)))
+        # sq.Fauna.zoo[darwin[d+reproductions][1]].mov.brain.layers = sq.Fauna.zoo[darwin[d][1]].mov.brain.layers
         sq.Fauna.zoo[darwin[d+reproductions][1]].mov.brain.mutate()
     for d in range(reproductions*2,population):
         sq.Fauna.zoo[darwin[d][1]] = sq.Creature(movement=RegisteredMov(pos=[100,100], box=Window, brain=Brain(schema=schema)))
